3eca43c464ba4297371504520103f19866ec6efc
syukri124 <syukriihsan680@gmail.com>
2025-12-13 01:11:42 +0700

    new

--- src/models/UserRole.js
+++ src/models/UserRole.js
@@ -1,8 +1,26 @@
 module.exports = (sequelize, DataTypes) => {
-  const UserRoles = sequelize.define("UserRoles", {
-    user_id: { type: DataTypes.INTEGER, allowNull: false },
-    role_id: { type: DataTypes.INTEGER, allowNull: false },
-  });
+  const UserRoles = sequelize.define(
+    "UserRoles",
+    {
+      user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      role_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "UserRoles",
+      timestamps: false, // <<< nonaktifkan createdAt & updatedAt
+    }
+  );
+
+  UserRoles.associate = (models) => {
+    UserRoles.belongsTo(models.Users, { foreignKey: "user_id" });
+    UserRoles.belongsTo(models.Roles, { foreignKey: "role_id" });
+  };
 
   return UserRoles;
 };
